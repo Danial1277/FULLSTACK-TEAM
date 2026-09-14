e9be8ee965fb9a365c9afdac419316df64d6ec0d
Danial1277 <kaigo6996@gmail.com>
2026-09-14 17:17:48 +0500

    изменил главную папку бэка

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,13 @@
+Import os
 import uvicorn
 from api import router as api_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-import os
 
 app = FastAPI(title="Multi-Tab Profile App")
 
-# Разрешаем запросы с фронтенда (CORS)
+# Настройка CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,18 +16,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Создаем папку uploads, если её нет
 os.makedirs("uploads", exist_ok=True)
-
-# Монтируем папку для загруженных фото
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
-# Подключаем API маршруты из api.py
-app.include_router(api_router, prefix="/api")
+# Подключаем API роуты
+app.include_router(api_router)
 
-# Монтируем фронтенд (HTML, CSS, JS)
-# ВАЖНО: подключается последним, чтобы не перекрывать /api и /uploads
-app.mount("/", StaticFiles(directory="../frontend", html=True), name="frontend")
+# Подключаем статику фронтенда из подпапки frontend
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
